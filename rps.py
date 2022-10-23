4ad8b6611335a5083bf0bc3249a9a6b884a1a9d1
Heaven Aldrico <4d3.h34v3n@gmail.com>
2022-10-23 18:38:31 +0700

    add: rock, paper, scissors

---
+++ rps.py
@@ -0,0 +1,37 @@
+import random
+  
+
+def get_choices():
+    options = ['rock', 'paper', 'scissors']
+    player_choice = input('Enter your choice (rock, paper, scissors): ')
+    computer_choice = random.choice(options)
+
+    return {'player': player_choice, 'computer': computer_choice}
+
+
+def check_win(player, computer):
+    print(f'You chose {player}, Computer chose {computer}')
+
+    if player == computer:
+        return "It's a tie!"
+    elif player == 'rock':
+        if computer == 'scissors':
+            return 'Rock smashes scissors! You win!'
+        else:
+            return 'Paper covers rock! You lose.'
+    elif player == 'paper':
+        if computer == 'rock':
+            return 'Paper covers rock! You win!'
+        else:
+            return 'Scissors cuts paper! You lose.'
+    elif player == 'scissors':
+        if computer == 'paper':
+            return 'Scissors cuts paper! You win!.'
+        else:
+            return 'Rock smashes scissors! You lose'
+
+
+choices = get_choices()
+result = check_win(choices['player'], choices['computer'])
+
+print(result)
